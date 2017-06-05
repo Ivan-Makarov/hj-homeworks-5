29a5a71cc1c4a8dde25a7519ec4fb6836c7b95ed
Ivan Makarov <ivan.s.makarov@yandex.ru>
2017-06-05 21:29:49 +0300

    исправлено

--- xhr/books-catalog/js/getbooks.js
+++ xhr/books-catalog/js/getbooks.js
@@ -2,7 +2,7 @@
 
 const request = new XMLHttpRequest();
 const catalogue = document.querySelector('#content');
-catalogue.innerHTML = "";
+catalogue.innerHTML = '';
 
 request.open('GET', 'https://netology-fbb-store-api.herokuapp.com/book/')
 request.send();
@@ -10,19 +10,15 @@ request.send();
 request.addEventListener('load', () => {
     let bookList = JSON.parse(request.responseText)
 
-    bookList.forEach(book => {
-        catalogue.innerHTML += '<li ' +
-        'data-title="' +
-        book.title +
-        '" data-author="' +
-        book.author.name +
-        '" data-info="' +
-        book.info +
-        '" data-price="' +
-        book.price +
-        '">' +
-        '<img src="' +
-        book.cover.small +
-        '"></li>';
-    })
+    function getBookInfo(book) {
+        let bookInfo = `<li data-title="${book.title}" data-author="${book.author.name}" data-info="${book.info}" data-price="${book.price}"><img src="${book.cover.small}"></li>`;
+
+        return bookInfo
+    }
+
+    let catalogueInfo = bookList.map(getBookInfo);
+
+    let catalogueHTML = catalogueInfo.join('');
+
+    catalogue.innerHTML = catalogueHTML;
 });
